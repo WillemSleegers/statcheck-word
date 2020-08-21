9ae0bd14d3f4b75e83170a931f8fc1f9d1a631de
Willem Sleegers <w.sleegers@me.com>
2020-08-21 15:44:35 +0200

    Prepared minimal add-in page for statcheck website.

--- word-add-in.php
+++ word-add-in.php
@@ -21,44 +21,12 @@
     <![endif]-->
   </head>
   <body>
-	<?php $pageid="documentation"; include 'navbar.php'; ?>
+	<?php $pageid="word-add-in"; include 'navbar.php'; ?>
     <div class="container">
       <div class="page-header" style="margin-top: 80px; border-bottom:0px;">
         <h1>MS Word Add-In</h1>
-        <p class="lead">Details</p>
-			<p>statcheck uses regular expressions to find statistical results in APA format. When a statistical result deviates from APA format, statcheck will not find it. The APA formats that statcheck uses are: t(df) = value, p = value; F(df1,df2) = value, p = value; r(df) = value, p = value; [chi]2 (df, N = value) = value, p = value (N is optional, delta G is also included); Z = value, p = value. All regular expressions take into account that test statistics and p values may be exactly (=) or inexactly (< or >) reported. Different spacing has also been taken into account.</p>
-			<p>Note that the conversion from PDF (and sometimes also HTML) to plain text and extraction of statistics can result in errors. Some statistical values can be missed, especially if the notation is unconventional. It is recommended to manually check some of the results.</p>
-			<p>Also, note that a seemingly inconsistent p value can still be correct when we take into account that the test statistic might have been rounded after calculating the corresponding p value. For instance, a reported t value of 2.35 could correspond to an actual value of 2.345 to 2.354 with a range of p values that can slightly deviate from the recomputed p value. statcheck will not count cases like this as errors.</p>
-			<p>The web implementation of statcheck will return an error if a PDF that does not contain any statistical results in APA format is submitted.</p>
-			<p>For more detailed information about statcheck can and cannot do, please see the online manual here: rpubs.com/michelenuijten/statcheckmanual.</p>
-		<p class="lead">Output</p>
-			<table class="table table-striped">
-				<thead>
-					<tr>
-						<th>Value</th>
-						<th>Description</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<td>Source</td>
-						<td>Name of the file of which the statistic is extracted.</td>
-					</td>
-					<tr>
-						<td>Statistical Reference</td>
-						<td>Raw string of the statistical reference that is extracted.</td>
-					</td>
-					<tr>
-						<td>Computed p Value</td>
-						<td>The recomputed p-value based on the reported degrees of freedom and test statistic.</td>
-					</td>
-					<tr>
-						<td>Consistency</td>
-						<td>Does the reported p-value match the recomputed p-value? (Consistency: the reported p-value matches the recomputed p-value. Inconsistency: the reported p-value does not match the recomputed p-value. Decision inconsistency: the reported p-value is statistically significant and the recomputed p-value is not, or vice versa.)</td>
-					</td>
-			</table>
-			<br/>
-			<br/>
+      
+		<p>We made a add-in for MS Word. It's cool.</p>
 		
 	  </div>
 	<?php include 'footer.php'; ?>
